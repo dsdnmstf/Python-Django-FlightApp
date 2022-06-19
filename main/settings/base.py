c4c44b35301d1435abfc04138e7f322682e9fb6f
musrafa <dsdnmstf@gmail.com>
2022-06-19 23:02:17 +0200

    settings seperated

--- main/settings/base.py
+++ main/settings/base.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,9 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #My apps
+
+    #Third party
+    "rest_framework",
+    'drf_yasg',
+    
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -70,16 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'main.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -122,3 +118,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
---
+++ main/settings/dev.py
@@ -0,0 +1,34 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+THIRD_PARTY_APPS = [
+    "debug_toolbar",
+]
+
+INSTALLED_APPS += THIRD_PARTY_APPS
+
+
+THIRD_PARTY_MIDDLEWARE = [
+    
+    #for debugs toolbar
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+MIDDLEWARE += THIRD_PARTY_MIDDLEWARE
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
