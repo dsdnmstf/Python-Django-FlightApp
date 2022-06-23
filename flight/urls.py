1b28bce9da188ac8b1beb4e8d01197781ca2dd60
musrafa <dsdnmstf@gmail.com>
2022-06-23 12:38:02 +0200

    reservation urls added

--- flight/urls.py
+++ flight/urls.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
-from .views import FlightView
+from .views import FlightView, Reservationview
 
 router = routers.DefaultRouter()
 router.register('flights', FlightView)
+router.register('reservation', Reservationview)
 
 
 urlpatterns = [
